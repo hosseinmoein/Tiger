406a54f71234936a3a4d447ceee97413bb6e6780
Hossein Moein <31854960+hosseinmoein@users.noreply.github.com>
2018-04-20 14:55:14 -0400

    Update Matrix.h

--- include/Matrix.h
+++ include/Matrix.h
@@ -1,6 +1,6 @@
 // Hossein Moein
-// August 1, 2009
-// Copyright (C) 2017-2018 Hossein Moein
+// February 11, 2018
+// Copyright (C) 2018-2019 Hossein Moein
 // Distributed under the BSD Software License (see file License)
 
 #ifndef _INCLUDED_Matrix_h
