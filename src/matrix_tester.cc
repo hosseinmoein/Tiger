c75dcd5034d6f82ce25ba190ae72d89eb446b07a
hossein.moein@kensho.com <hossein.moein@kensho.com>
2020-01-15 09:41:01 -0500

    Added license statement to all source files

--- src/matrix_tester.cc
+++ src/matrix_tester.cc
@@ -1,12 +1,39 @@
 // Hossein Moein
 // February 11, 2018
-// Copyright (C) 2018-2019 Hossein Moein
-// Distributed under the BSD Software License (see file License)
+/*
+Copyright (c) 2019-2022, Hossein Moein
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+* Redistributions of source code must retain the above copyright
+  notice, this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer in the
+  documentation and/or other materials provided with the distribution.
+* Neither the name of Hossein Moein and/or the DataFrame nor the
+  names of its contributors may be used to endorse or promote products
+  derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 #include <iostream>
+#include <time.h>
 
 #include <BaseMathOperators.h>
 #include <MathOperators.h>
+
+#include <Eigen/Dense>
 
 #include <Complex.h>
 #include <Matrix.h>
@@ -1582,6 +1609,44 @@ int main (int argCnt, char *argVctr [])  {
                   << c2.transform(trans_func) << std::endl;
 
         std::cout.precision (pre);
+    }
+    {
+        std::cout << "\nTesting Eigen performace ...\n" << std::endl;
+
+        const size_t    dem = 500;
+
+        Eigen::MatrixXd emat(dem, dem);
+        DDMatrix        hmat (dem, dem);
+        int             count = 0;
+
+        for (DDMatrix::size_type i = 0; i < dem; ++i)  {
+            for (DDMatrix::size_type j = 0; j < dem; ++j)  {
+                emat (i, j) = double(++count);
+                hmat (i, j) = double(count);
+            }
+        }
+
+        const time_t    hstart = time(nullptr);
+
+        for (DDMatrix::size_type i = 0; i < 10; ++i)  {
+            DDMatrix    result = hmat * hmat * hmat;
+
+            result(3, 4) = 123.4;
+        }
+
+        const time_t    hend = time(nullptr);
+        const time_t    estart = time(nullptr);
+
+        for (DDMatrix::size_type i = 0; i < 10; ++i)  {
+            Eigen::MatrixXd result = emat * emat * emat;
+
+            result(3, 4) = 123.4;
+        }
+
+        const time_t    eend = time(nullptr);
+
+        std::cout << "Eigen took: " << eend - estart << std::endl
+                  << "Hossein Took: " << hend - hstart << std::endl;
     }
 
     // {
